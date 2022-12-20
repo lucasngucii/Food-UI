c42d3c0ed058d0c96d7924bf919cb87be9da3824
An Ne` <daenvle69@gmail.com>
2022-12-20 21:24:03 +0700

    20/12/2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './components/partials/header/header.component';
-import {HomeComponent} from './components/pages/home/home.component';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
 import { StarComponent } from './shared/star/star.component';
-import {ConvertToSpacesPipe} from "./shared/star/convert-to-spaces.pipe";
+import { ConvertToSpacesPipe } from './shared/star/convert-to-spaces.pipe';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagComponent } from './components/partials/tag/tag.component';
 import { ProductDetailComponent } from './components/pages/product-detail/product-detail.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
 @NgModule({
   declarations: [
@@ -21,15 +21,12 @@ import { ProductDetailComponent } from './components/pages/product-detail/produc
     ConvertToSpacesPipe,
     SearchComponent,
     TagComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    CartPageComponent,
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    /*RatingModule,*/
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
